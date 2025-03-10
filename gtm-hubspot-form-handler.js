62c913fbc7c1610eeda5791cbc86b139c7f4b3d4
ayo_nci <78206286+ayo-nci@users.noreply.github.com>
2025-03-10 19:31:57 +0000

    Update gtm-hubspot-form-handler.js
    Changed 'const' to 'var' to support GTM versions

--- gtm-hubspot-form-handler.js
+++ gtm-hubspot-form-handler.js
@@ -5,45 +5,39 @@
  */
 
 <script type="text/javascript">
-  //Load datalayer if not available
- // window.dataLayer = window.dataLayer || [];
-  
-  window.addEventListener("message", function(event) {
-    if (!event.data) return;
-    
-    // For new V3/V4 forms
-    if (event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormSubmitted') {
-      const formContainer = document.querySelector('.hs-form-frame');
-      const formId = formContainer ? formContainer.getAttribute('data-form-id') : event.data.id;
-      
-      window.dataLayer.push({
-        'event': 'hubspot-form-success',
-        'hs-form-guid': formId
-      });
-    }
-    
-    // For legacy forms
-    if (event.data.type === 'HS_CTA_FORM_SUBMITTED') {
-      window.dataLayer.push({
-        'event': 'hubspot-form-success',
-        'hs-form-guid': event.data.id || event.data.formId
-      });
-    }
-  });
-  
-  // For the newest V4 API implementation
-  if (document) {
-    document.addEventListener('hs-form-event:on-submission:success', function(event) {
-      const formContainer = document.querySelector('.hs-form-frame');
-      const formId = formContainer ? formContainer.getAttribute('data-form-id') : 
-                    (event.detail && event.detail.formId ? event.detail.formId : null);
-      
-      if (formId) {
-        window.dataLayer.push({
-          'event': 'hubspot-form-success',
-          'hs-form-guid': formId
-        });
-      }
+    //window.dataLayer = window.dataLayer || [];
+    window.addEventListener("message", function (event) {
+        if (!event.data) return;
+        // For old legacy forms (v1)
+        if (event.data.type === 'HS_CTA_FORM_SUBMITTED') {
+            window.dataLayer.push({
+                'event': 'hubspot-form-success',
+                'hs-form-guid': event.data.id || event.data.formId
+            });
+        }
+        // For recent legacy forms (v3)
+        if (event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormSubmitted') {
+            var formContainer = document.querySelector('.hs-form-frame');
+            var formId = formContainer ? formContainer.getAttribute('data-form-id') : event.data.id;
+            window.dataLayer.push({
+                'event': 'hubspot-form-success',
+                'hs-form-guid': formId
+            });
+        }
+        
     });
-  }
+    // For the latest forms (v4 api)
+    if (document) {
+        document.addEventListener('hs-form-event:on-submission:success', function (event) {
+            var formContainer = document.querySelector('.hs-form-frame');
+            var formId = formContainer ? formContainer.getAttribute('data-form-id') :
+                (event.detail && event.detail.formId ? event.detail.formId : null);
+            if (formId) {
+                window.dataLayer.push({
+                    'event': 'hubspot-form-success',
+                    'hs-form-guid': formId
+                });
+            }
+        });
+    }
 </script>
